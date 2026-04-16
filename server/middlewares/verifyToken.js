8e040ff5acf80a0f2ba856d19ddfa8925d26e230
Camilo <166083316+JCamiloOg@users.noreply.github.com>
2026-04-15 20:09:30 -0500

    FEAT: Ahora la autenticación funciona dependiendo del rol

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -2,9 +2,10 @@ import jwt from "jsonwebtoken";
 import { findUserByDocument } from "../models/users.model.js";
 
 
-export async function verifyTokenAdmin(req, res, next) {
+export async function authenticate(req, res, next) {
 
     try {
+
         const { token } = req.cookies;
 
         if (!token) return res.status(401).json({ message: "Acceso no autorizado." });
@@ -17,38 +18,25 @@ export async function verifyTokenAdmin(req, res, next) {
 
         if (!user) return res.status(401).json({ message: "Acceso no autorizado." });
 
-        if (user.estado === 0) return res.status(401).json({ message: "Acceso no autorizado." });
+        req.user = user;
 
-        if (user.role_id !== 1) return res.status(401).json({ message: "Acceso no autorizado." });
+        if (user.estado === 0) return res.status(401).json({ message: "Acceso no autorizado." });
 
         next();
     } catch (error) {
         console.log(error);
+
+        if (error.name === "TokenExpiredError") return res.status(401).json({ message: "Sesión expirada inicia sesión de nuevo." });
+
+
         return res.status(500).json({ message: "Error al verificar el token." });
     }
 }
 
-export async function verifyTokenInCharge(req, res, next) {
-    try {
-        const { token } = req.cookies;
-
-        if (!token) return res.status(401).json({ message: "Acceso no autorizado." });
-
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
-        if (!decoded) return res.status(401).json({ message: "Acceso no autorizado." });
-
-        const user = await findUserByDocument(decoded.id);
-
-        if (!user) return res.status(401).json({ message: "Acceso no autorizado." });
-
-        if (user.estado === 0) return res.status(401).json({ message: "Acceso no autorizado." });
-
-        if (user.role_id !== 2) return res.status(401).json({ message: "Acceso no autorizado." });
+export function authenticateRoles(...allowedRoles) {
+    return (req, res, next) => {
+        if (!allowedRoles.includes(req.user.rol)) return res.status(401).json({ message: "Acceso no autorizado." });
 
         next();
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Error al verificar el token." });
-    }
+    };
 }
